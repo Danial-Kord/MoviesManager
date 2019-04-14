2d779474df5920b3d283b8ba2c668ed6055e935d
Danial-Kord <33172110+Danial-Kord@users.noreply.github.com>
2019-04-15 01:18:57 +0430

    new class added this class clean the folder by citing any movies to the main folder then deleting all empty remaining folders

---
+++ src/com/company/DirCleaner.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class DirCleaner {
+    //    public static String buildNewFolder(String path){
+//        Random random = new Random();
+//        int a = random.nextInt(2000);
+//        return buildNewFolder(path,"Dan20");
+//    }
+    public static String buildNewFolder(String path){
+        File folder = new File(path);
+        if(folder.isDirectory())
+        {
+            ArrayList<File>fileList = new ArrayList<File>();
+            fileList = getMovies(folder,fileList);
+
+            System.out.println("\nTotal number of items present in the directory: " + fileList.size() );
+
+            File dir = new File(path);
+//            dir.mkdir();
+            // Lists only files since we have applied file filter
+            for(File file:fileList)
+            {
+                if(file.getName().endsWith(".mkv") || file.getName().endsWith(".mp4") || file.getName().endsWith(".mpeg") ||
+                        file.getName().endsWith(".mpeg2") || file.getName().endsWith(".avi")) {
+                    if(!(dir.getAbsolutePath() + "\\" + file.getName()).equals(file.getAbsolutePath())) {
+                        File destination = new File(dir.getAbsolutePath() + "\\" + file.getName());
+                        if (file.renameTo(destination)) {
+                            file.delete();
+                        }
+                    }
+                }
+            }
+        }
+        deletDirs(folder);
+        return path;
+    }
+    private static ArrayList<File> getMovies(File dad,ArrayList<File>files){
+        File[] fileList = dad.listFiles();
+        for (File temp : fileList){
+            if(temp.isDirectory()){
+                files = getMovies(temp,files);
+            }
+            else {
+                files.add(temp);
+            }
+        }
+        return files;
+    }
+    private static void deletDirs(File folder){
+        if(folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    if (file.delete())
+                        continue;
+                    else{
+                        deletDirs(file);
+                    }
+                    file.delete();
+                }
+            }
+        }
+    }
+}
+
