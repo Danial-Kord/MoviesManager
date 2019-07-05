264c900775798e30604396c48d75072555fb9a4e
danial-kord <daial.kordmodanlou@gmail.com>
2019-07-05 14:35:09 -0700

    menu and logo added . some problem with placements

--- src/UI/Gui.java
+++ src/UI/Gui.java
@@ -2,14 +2,25 @@ package UI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-import javafx.scene.control.Button;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Gui extends Application {
@@ -20,7 +31,7 @@ public class Gui extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        GridPane root = new GridPane();
+        BorderPane root = new BorderPane();
 //        try {
 //            root = FXMLLoader.load(getClass().getResource("danial.fxml"));
 //        } catch (IOException e) {
@@ -28,9 +39,46 @@ public class Gui extends Application {
 //        }
 
         primaryStage.setTitle("Hello World");
-        root.getStylesheets().add("UI/Danial.css");
+
         primaryStage.setScene(new Scene(root, 1200, 600));
-        root.setGridLinesVisible(true);
+
+        //menu
+        MenuBar menubar = new MenuBar();
+        Menu FileMenu = new Menu("File");
+        MenuItem filemenu1=new MenuItem("new");
+        MenuItem filemenu2=new MenuItem("Save");
+        MenuItem filemenu3=new MenuItem("Exit");
+        Menu EditMenu=new Menu("Edit");
+        MenuItem EditMenu1=new MenuItem("Cut");
+        MenuItem EditMenu2=new MenuItem("Copy");
+        MenuItem EditMenu3=new MenuItem("Paste");
+        EditMenu.getItems().addAll(EditMenu1,EditMenu2,EditMenu3);
+        FileMenu.getItems().addAll(filemenu1,filemenu2,filemenu3);
+        menubar.getMenus().addAll(FileMenu,EditMenu);
+        //set menu position
+        BorderPane top = new BorderPane();
+        top.setTop(menubar);
+
+        //image logo
+        FileInputStream input= null;
+        try {
+            input = new FileInputStream("D:\\Danial\\Projects\\InteliJ Idea\\MoviesManager\\src\\pics\\danialArts.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image = new Image(input);
+        ImageView imageview=new ImageView(image);
+        imageview.setFitWidth(300);
+        imageview.setFitHeight(200);
+        Label my_label=new Label("",imageview);
+        my_label.setId("logo");
+        top.setId("top");
+        top.setRight(my_label);
+
+
+        root.setTop(top);
+
         primaryStage.show();
+        root.getStylesheets().add("UI/Danial.css");
     }
 }
